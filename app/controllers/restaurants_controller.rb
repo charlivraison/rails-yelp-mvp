cfd25406ee6d050755e0311e0ec669605f750b2c
Charly Larere <charlylarerepro@outlook.fr>
2024-11-13 02:06:40 +0100

    modif

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,2 +1,41 @@
 class RestaurantsController < ApplicationController
+  before_action :set_restaurant, only: [ :show, :edit, :update, :destroy ]
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def show
+  end
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def edit
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    if @restaurant.save
+      redirect_to @restaurant, notice: "Restaurant was successfully created."
+    else
+      render :new
+    end
+  end
+
+  def update
+    @restaurant.update(restaurant_params)
+  end
+
+  def destroy
+    @restaurant.destroy
+  end
+  private
+  # Use callbacks to share common setup or constraints between actions.
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :phone_number, :category)
+  end
 end
